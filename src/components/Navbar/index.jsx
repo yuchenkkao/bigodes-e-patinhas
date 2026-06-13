eb73dc88e7249045c1faa5ca5bae36cc0ab250a3
yuchenkkao <juliakaocoisas@gmail.com>
2026-06-12 22:51:24 -0300

    colocando forma na navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,6 +4,7 @@ import logoImg from '../../assets/NavbarLogo.png';
 
 export default function Navbar() {
   return (
+    <>
     <nav>
      <div>
         <img src={logoImg} alt="Logotipo Bigodes e Patinhas" className="logo" />
@@ -14,8 +15,19 @@ export default function Navbar() {
         <li><Link to="/agenda" className="link">Agenda</Link></li>
         <li><Link to="/tutores" className="link">Tutores</Link></li>
         <li><Link to="/pets" className="link">Pets</Link></li>
-        <li><Link to="/meus-agendamentos" className="link">Área do Cliente</Link></li>
+        <li><Link to="/meus-agendamentos" className="link">Agendamentos</Link></li>
       </ul>
     </nav>
+
+    <div className="onda-container">
+        <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+         <path 
+          fill="#4068DE" 
+          fillOpacity="1" 
+          d="M0,64L48,58.7C96,53,192,43,288,48C384,53,480,75,576,80C672,85,768,75,864,58.7C960,43,1056,21,1152,26.7C1248,32,1344,64,1392,80L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+  ></path>
+        </svg>
+      </div>
+      </>
   );
 }
