07aec95482bb8690bc1247ff7b7a628ce6a70e0d
yuchenkkao <juliakaocoisas@gmail.com>
2026-06-15 09:00:32 -0300

    arrumando token da navbar

---
+++ src/components/MenuPerfil/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from "react-icons/fa";
+import './styles.css';
+
+export default function MenuPerfil() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const alternarMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
+  const simularLogin = (modo) => {
+    localStorage.setItem('@BigodesToken', modo);  
+    setMenuAberto(false); 
+    window.location.reload()
+  };
+
+  return (
+    <div className="perfil-container">
+      <button onClick={alternarMenu} className="link-perfil">
+        <FaUserCircle />
+      </button>
+
+      {menuAberto && (
+        <div className="dropdown-menu">
+          <Link to="/perfil" onClick={() => setMenuAberto(false)}>Meu Perfil</Link>
+          <Link to="/login" onClick={() => setMenuAberto(false)}>Página de Login</Link>
+          <button onClick={() => simularLogin('visitante')}>Sair</button>
+
+          <div className="divisor"></div>
+          
+          <span className="dropdown-titulo">Modo</span>
+          <button onClick={() => simularLogin('cliente')}>Modo Cliente</button>
+          <button onClick={() => simularLogin('atendente')}>Modo Atendente</button>
+          <button onClick={() => simularLogin('veterinario')}>Modo Veterinário</button>
+          <button onClick={() => simularLogin('gestor')}>Modo Gestor</button>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,33 +1,52 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 import logoImg from '../../assets/NavbarLogo.png';
+
+import MenuPerfil from '../MenuPerfil';
 
 import { MdOutlinePets } from "react-icons/md";
 import { FaClock } from "react-icons/fa6";
 import { IoStatsChart } from "react-icons/io5";
 import { FaHome } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
-import { FaUserCircle } from "react-icons/fa";
+import { LuLogIn } from "react-icons/lu";
+
 
 export default function Navbar() {
+
+  const token = localStorage.getItem('@BigodesToken') || 'visitante';
+
   return (
     <>
     <nav>
      <div>
         <img src={logoImg} alt="Logotipo Bigodes e Patinhas" className="logo" />
-      </div>
+    </div>
 
       <ul className="lista">
         <li><Link to="/" className="link"><FaHome /> Página Inicial </Link></li>
+        {token !== 'visitante' && (
         <li><Link to="/agenda" className="link"><FaClock /> Agenda</Link></li>
+        )}
+
+        {token === 'visitante' && (
+        <li><Link to="/login" className="link"><LuLogIn /> Login</Link></li>
+        )}
+
+        {(token === 'atendente' || token === 'veterinario' || token === 'gestor') && (
+          <>
         <li><Link to="/tutores" className="link"><BsFillPeopleFill /> Tutores</Link></li>
         <li><Link to="/pets" className="link"><MdOutlinePets /> Pets </Link> </li>
+          </>)}
+
+        {(token === 'gestor') && (
         <li><Link to="/relatorios" className="link"><IoStatsChart /> Relatórios da Clínica</Link></li>
+        )}
       </ul>
 
-        <div className="perfil-container">
-          <Link to="/perfil" className="link-perfil" > <FaUserCircle /> </Link>
-        </div>
+      <MenuPerfil/>
+
     </nav>
 
     <div className="onda-container">
@@ -38,7 +57,7 @@ export default function Navbar() {
             d="M0,128L48,138.7C96,149,192,171,288,160C384,149,480,107,576,106.7C672,107,768,149,864,165.3C960,181,1056,171,1152,149.3C1248,128,1344,96,1392,80L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
           ></path>
         </svg>
-      </div>
-      </>
+    </div>
+    </>
   );
 }
